db41877fadc3a428da71dfd23fdd14fa24240299
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-08-28 11:40:52 +0530

    Register profile compensation Room migration

--- app/src/main/java/com/pasindu/nursingotapp/data/local/DatabaseProvider.kt
+++ app/src/main/java/com/pasindu/nursingotapp/data/local/DatabaseProvider.kt
@@ -25,7 +25,8 @@ object DatabaseProvider {
                 AppDatabase.MIGRATION_2_3,
                 AppDatabase.MIGRATION_1_3,
                 AppDatabase.MIGRATION_3_4,
-                AppDatabase.MIGRATION_4_5
+                AppDatabase.MIGRATION_4_5,
+                AppDatabase.MIGRATION_5_6
             )
             .fallbackToDestructiveMigration()
             .build()
