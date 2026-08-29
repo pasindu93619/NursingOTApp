b525d99ab41c50d539791fde16644deca957c174
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-08-29 11:36:57 +0530

    Restore single Hilt DaoModule for DAO bindings

--- app/src/main/java/com/pasindu/nursingotapp/di/DaoModule.kt
+++ app/src/main/java/com/pasindu/nursingotapp/di/DaoModule.kt
@@ -1,6 +1,15 @@
 package com.pasindu.nursingotapp.di
 
 import com.pasindu.nursingotapp.data.local.AppDatabase
+import com.pasindu.nursingotapp.data.local.dao.ClaimPeriodDao
+import com.pasindu.nursingotapp.data.local.dao.DailyEntryDao
+import com.pasindu.nursingotapp.data.local.dao.FinancialDao
+import com.pasindu.nursingotapp.data.local.dao.KnowledgeHubDao
+import com.pasindu.nursingotapp.data.local.dao.PayRateSettingsDao
+import com.pasindu.nursingotapp.data.local.dao.ProfileCompensationDao
+import com.pasindu.nursingotapp.data.local.dao.ProfileDao
+import com.pasindu.nursingotapp.data.local.dao.SalaryStep2027Dao
+import com.pasindu.nursingotapp.data.local.dao.PaySheetDocumentDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,32 +20,32 @@ import dagger.hilt.components.SingletonComponent
 object DaoModule {
 
     @Provides
-    fun provideProfileDao(database: AppDatabase) = database.profileDao()
+    fun provideProfileDao(database: AppDatabase): ProfileDao = database.profileDao()
 
     @Provides
-    fun provideClaimPeriodDao(database: AppDatabase) = database.claimPeriodDao()
+    fun provideClaimPeriodDao(database: AppDatabase): ClaimPeriodDao = database.claimPeriodDao()
 
     @Provides
-    fun provideDailyEntryDao(database: AppDatabase) = database.dailyEntryDao()
+    fun provideDailyEntryDao(database: AppDatabase): DailyEntryDao = database.dailyEntryDao()
 
     @Provides
-    fun provideFinancialDao(database: AppDatabase) = database.financialDao()
+    fun provideFinancialDao(database: AppDatabase): FinancialDao = database.financialDao()
 
     @Provides
     fun provideClinicalPlanningDao(database: AppDatabase) = database.clinicalPlanningDao()
 
     @Provides
-    fun provideKnowledgeHubDao(database: AppDatabase) = database.knowledgeHubDao()
+    fun provideKnowledgeHubDao(database: AppDatabase): KnowledgeHubDao = database.knowledgeHubDao()
 
     @Provides
-    fun providePayRateSettingsDao(database: AppDatabase) = database.payRateSettingsDao()
+    fun providePayRateSettingsDao(database: AppDatabase): PayRateSettingsDao = database.payRateSettingsDao()
 
     @Provides
-    fun provideProfileCompensationDao(database: AppDatabase) = database.profileCompensationDao()
+    fun provideProfileCompensationDao(database: AppDatabase): ProfileCompensationDao = database.profileCompensationDao()
 
     @Provides
-    fun provideSalaryStep2027Dao(database: AppDatabase) = database.salaryStep2027Dao()
+    fun provideSalaryStep2027Dao(database: AppDatabase): SalaryStep2027Dao = database.salaryStep2027Dao()
 
     @Provides
-    fun providePaySheetDocumentDao(database: AppDatabase) = database.paySheetDocumentDao()
+    fun providePaySheetDocumentDao(database: AppDatabase): PaySheetDocumentDao = database.paySheetDocumentDao()
 }
