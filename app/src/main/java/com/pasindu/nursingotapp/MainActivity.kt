99a1b6834837a181655239408da87bcce4947b46
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-08-29 12:12:43 +0530

    Fix Hilt activity integration for ViewModel creation

--- app/src/main/java/com/pasindu/nursingotapp/MainActivity.kt
+++ app/src/main/java/com/pasindu/nursingotapp/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.pasindu.nursingotapp.ui.navigation.AppNavigation
 import com.pasindu.nursingotapp.ui.theme.NursingOTAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
