fdbdbb2edab87617bfc3c71225e40b8677dd7662
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-09-03 04:02:15 +0530

    test: add Room migration test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.room.testing)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
