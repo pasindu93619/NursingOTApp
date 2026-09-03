a4b2757e4b064631dc221f4e5b358baca86817ab
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-09-03 16:39:08 +0530

    Step 1.4: Register analytics boundary use cases

--- app/src/main/java/com/pasindu/nursingotapp/di/UseCaseModule.kt
+++ app/src/main/java/com/pasindu/nursingotapp/di/UseCaseModule.kt
@@ -7,9 +7,7 @@ import com.pasindu.nursingotapp.data.local.dao.PayRateSettingsDao
 import com.pasindu.nursingotapp.data.local.dao.ProfileCompensationDao
 import com.pasindu.nursingotapp.data.local.dao.ProfileDao
 import com.pasindu.nursingotapp.data.local.dao.SalaryStep2027Dao
-import com.pasindu.nursingotapp.domain.usecase.AddClinicalTaskUseCase
 import com.pasindu.nursingotapp.domain.usecase.AddCpdLogUseCase
-import com.pasindu.nursingotapp.domain.usecase.AddIsbarNoteUseCase
 import com.pasindu.nursingotapp.domain.usecase.ApplyMatched2027DayRateUseCase
 import com.pasindu.nursingotapp.domain.usecase.CalculateDailyEntryHoursUseCase
 import com.pasindu.nursingotapp.domain.usecase.CalculateFinanceSummaryUseCase
@@ -19,22 +17,20 @@ import com.pasindu.nursingotapp.domain.usecase.DeleteClaimPeriodUseCase
 import com.pasindu.nursingotapp.domain.usecase.EnsureManualPayRateRecordUseCase
 import com.pasindu.nursingotapp.domain.usecase.GetDailyEntryForDateUseCase
 import com.pasindu.nursingotapp.domain.usecase.MatchSalaryStepUseCase
+import com.pasindu.nursingotapp.domain.usecase.ObserveAllDailyEntriesUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveClaimDailyEntriesUseCase
+import com.pasindu.nursingotapp.domain.usecase.ObserveClaimPeriodsForAnalyticsUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveClaimPeriodsUseCase
-import com.pasindu.nursingotapp.domain.usecase.ObserveClinicalTasksUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveCpdLogsUseCase
-import com.pasindu.nursingotapp.domain.usecase.ObserveIsbarNotesUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveOtRateUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveProfileCompensationUseCase
 import com.pasindu.nursingotapp.domain.usecase.ObserveProfileUseCase
-import com.pasindu.nursingotapp.domain.usecase.PurgeOldIsbarNotesUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveDailyEntryUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveFinanceCompensationUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveFinanceRatesUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveOtRateUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveProfileCompensationUseCase
 import com.pasindu.nursingotapp.domain.usecase.SaveProfileUseCase
-import com.pasindu.nursingotapp.domain.usecase.SetClinicalTaskCompletedUseCase
 import com.pasindu.nursingotapp.domain.usecase.SynchronizePolicyRatesUseCase
 import dagger.Module
 import dagger.Provides
@@ -67,10 +63,6 @@ object UseCaseModule {
     @Provides fun provideGetDailyEntryForDateUseCase(dao: DailyEntryDao) = GetDailyEntryForDateUseCase(dao)
     @Provides fun provideObserveCpdLogsUseCase(dao: KnowledgeHubDao) = ObserveCpdLogsUseCase(dao)
     @Provides fun provideAddCpdLogUseCase(dao: KnowledgeHubDao) = AddCpdLogUseCase(dao)
-    @Provides fun provideObserveIsbarNotesUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = ObserveIsbarNotesUseCase(dao)
-    @Provides fun provideObserveClinicalTasksUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = ObserveClinicalTasksUseCase(dao)
-    @Provides fun provideAddIsbarNoteUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = AddIsbarNoteUseCase(dao)
-    @Provides fun providePurgeOldIsbarNotesUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = PurgeOldIsbarNotesUseCase(dao)
-    @Provides fun provideAddClinicalTaskUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = AddClinicalTaskUseCase(dao)
-    @Provides fun provideSetClinicalTaskCompletedUseCase(dao: com.pasindu.nursingotapp.data.local.dao.ClinicalPlanningDao) = SetClinicalTaskCompletedUseCase(dao)
+    @Provides fun provideObserveAllDailyEntriesUseCase(dao: DailyEntryDao) = ObserveAllDailyEntriesUseCase(dao)
+    @Provides fun provideObserveClaimPeriodsForAnalyticsUseCase(dao: ClaimPeriodDao) = ObserveClaimPeriodsForAnalyticsUseCase(dao)
 }
