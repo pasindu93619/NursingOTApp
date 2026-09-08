37701aa69df293c27d141eaafdb8edcc8e30f0ef
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-09-09 00:53:26 +0530

    Fix HomeScreen system bar insets

--- app/src/main/java/com/pasindu/nursingotapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pasindu/nursingotapp/ui/screens/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -102,7 +103,10 @@ fun HomeScreen(
     }
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize().background(AppBackground),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppBackground)
+            .safeDrawingPadding(),
         contentPadding = PaddingValues(
             NursingDimensions.Spacing.lg,
             NursingDimensions.Spacing.lg,
