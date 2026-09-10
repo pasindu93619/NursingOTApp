c1c2da19b0c3a0b90e505fa54446334c62574745
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-09-11 04:29:01 +0530

    Fix AnalyticsScreen pill shape import

--- app/src/main/java/com/pasindu/nursingotapp/ui/screens/AnalyticsScreen.kt
+++ app/src/main/java/com/pasindu/nursingotapp/ui/screens/AnalyticsScreen.kt
@@ -67,6 +67,7 @@ import com.pasindu.nursingotapp.ui.theme.md_theme_light_onTertiaryContainer
 import com.pasindu.nursingotapp.ui.theme.md_theme_light_primaryContainer
 import com.pasindu.nursingotapp.ui.theme.md_theme_light_secondaryContainer
 import com.pasindu.nursingotapp.ui.theme.md_theme_light_tertiaryContainer
+import com.pasindu.nursingotapp.ui.theme.pill
 import kotlinx.coroutines.delay
 import java.time.DayOfWeek
 import java.time.LocalDate
